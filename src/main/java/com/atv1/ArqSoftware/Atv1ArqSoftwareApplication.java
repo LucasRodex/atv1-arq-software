d5aa3c49cdb26b30ebf31297c7a042f0f3b87741
lucas <lucascarlosrodex@gmail.com>
2025-02-12 22:45:13 -0300

    first commit

---
+++ src/main/java/com/atv1/ArqSoftware/Atv1ArqSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.atv1.ArqSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atv1ArqSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atv1ArqSoftwareApplication.class, args);
+	}
+
+}
